413d47783527756367738228067ea8d00a879793
Aaron Hernandez <linkable19@gmail.com>
2025-04-29 18:06:57 +0000

    just redo it and use gemini for ai

--- src/ai/ai-instance.ts
+++ src/ai/ai-instance.ts
@@ -2,11 +2,11 @@ import { genkit } from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
+  defaultModel: 'gemini-1.0-pro',
   promptDir: './prompts',
   plugins: [
     googleAI({
       apiKey: process.env.GOOGLE_GENAI_API_KEY,
     }),
   ],
-  
 });
